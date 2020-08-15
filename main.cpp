6b552f50a3e3895f30fc79873a564a2ece6a8d21
arrrrrr <i.am.the.ar@gmail.com>
2020-08-15 22:45:03 +1000

    Use boost program_options for command-line parsing

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <boost/program_options.hpp>
 #include "config/default_config.h"
 #include "config/reader.h"
 
-int main() {
+int main(int argc, char **argv) {
+    namespace po = boost::program_options;
 
     return 0;
 }
