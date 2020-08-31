20b736cfeae0829103fe9de67cb1b7427ea3197e
arrrrrr <i.am.the.ar@gmail.com>
2020-08-31 12:51:44 +1000

    Create common headers to include all the headers in the namespace

---
+++ app/common.h
@@ -0,0 +1,9 @@
+//
+// Created by mikey on 31/08/2020.
+//
+
+#pragma once
+#include "app/cli_option_parser.h"
+#include "app/console_logger.h"
+#include "app/constants.h"
+#include "app/file_exception.h"
---
+++ config/common.h
@@ -0,0 +1,10 @@
+//
+// Created by mikey on 31/08/2020.
+//
+
+#pragma once
+#include "config/config_factory.h"
+#include "config/constants.h"
+#include "config/default_config.h"
+#include "config/master_reader.h"
+#include "config/slave_reader.h"
